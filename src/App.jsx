b557e6769e2e808fba8ea3fd52d7cbe48f9aff7a
grandony <trinchvr@gmail.com>
2022-10-20 22:19:01 -0500

    actualizacion de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+
+
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    {title: "Inicio", src:"user"},
+    {title: "Empresas", src:"building-office-2"},
+    {title: "Personal", src:"user-group"},
+    {title: "Kardex", src:"clipboard-document-list"},
+  ]
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+      <>
 
-export default App
+      <div className="flex">
+
+        <div className={` ${open ? "w-72" : "w-20"} duration-300 h-screen p-5 pt-8 bg-dark-purple relative`}
+        >
+          
+          <img 
+            src="./src/heroicons/20/solid/chevron-left.svg"  
+            className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple bg-white ${!open && "rotate-180"}`}
+            onClick={()=> setOpen(!open)}
+          />
+          <div className="flex gap-x-4 items-center ">
+            <img 
+              src="./src/img/logo-1.png" 
+              alt="" 
+              className={`cursor-pointer duration-500 w-10`}
+            />
+            <h1 
+            className={`text-white origin-left font-medium text-xl duration-300 ${!open && "hidden" } `}
+            >
+              Electrosur S.A.
+            </h1>
+
+          </div>
+            <ul className="pt-6">
+              {Menus.map((menu, index)=>(
+                <li key={index} className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md">
+                  <img 
+                    src={`./src/heroicons/20/solid/${menu.src}.svg`} 
+                    alt="" 
+                    className="text-white bg-white rounded-full p-1"  
+                  />
+                  <span
+                    className={`${!open && 'hidden'} origin-left duration-200 text-lg`}
+                  >{menu.title}</span>
+                </li>
+              ) )}
+            </ul>
+          
+        </div>
+
+        <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+          <h1>Area de trabajo T_T pipipi</h1>
+        </div>
+      
+      </div>
+      </>
+         
+    
+    )
+} 
